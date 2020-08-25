591da8634ed17e1693463b44f1b18e50e310fb5c
TWCurry <thomas.w.curry@accenture.com>
2020-08-25 20:38:24 +0100

    Added launcher script

--- Source/OAir2json.py
+++ Source/OAir2json.py
@@ -1,1 +1,10 @@
-# Script to convert Open air format files to JSON
+# Function to convert Open air format files to JSON
+import sys
+
+def OAir2json():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    print("This file is a module only, please run main.py")
+    sys.exit(1)
---
+++ Source/main.py
@@ -0,0 +1,9 @@
+# Script to convert Open air format files to JSON
+import sys
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
