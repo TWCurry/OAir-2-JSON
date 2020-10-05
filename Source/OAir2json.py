6c5635b31ea5d9b051f1c6ffd85f69cd854d48da
TWCurry <thomas.w.curry@accenture.com>
2020-10-05 16:12:05 +0100

    Added intro section identification

--- Source/OAir2json.py
+++ Source/OAir2json.py
@@ -1,8 +1,21 @@
 # Function to convert Open air format files to JSON
-import sys
+import sys, json
+
+outputJson = {}
 
 def OAir2json(asFileContents):
-    print(asFileContents)
+    intro = True
+    introText = ""
+    for line in asFileContents.splitlines():
+        if list(line)[0] == "*": # If line is a comment
+            if intro == True:
+                introText += f"{line[2:]}\n"
+        else:
+            if intro == True: # Add intro text section
+                outputJson["introText"] = introText
+                intro = False
+
+    print(json.dumps(outputJson))
 
 
 if __name__ == "__main__":
